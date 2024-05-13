db1eecda49d35bfc71f633cb0c227f56f43b1575
CHORN.CHOAN <CHORN.CHOAN@PNC.LAN>
2024-05-13 17:28:45 +0700

    homework vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <FirstProject/>
+</template>
+
+<script>
+import FirstProject from './components/FirstProject.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FirstProject
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FirstProject.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+<div class="header w-250 ">
+        <h1 class="d-grid gap-2 col-3 mx-auto mt-5" >Tables</h1>
+          <div>
+            <input type="text" v-model="newUser.firstname" placeholder="FirstName">
+            <input type="TEXT" v-model="newUser.lastname" placeholder="LastName">
+            <input type="email" v-model="newUser.email" placeholder="Email">
+            <button class="btn btn-primary" v-if="newUsers in newUser !== 0" @click="addUser">Add User</button>
+        </div>
+        <h4 class="mt-4 text-danger" > Here is totalUsers:{{totalUsers}}</h4>
+
+        <table class="table mb-4">
+          <thead class="bg-primary"> 
+            <tr>
+              <th>ID</th >
+              <th>FirstName</th>
+              <th>LastName</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-colab" v-for="user in users" :key="user.id">
+              <td>{{ user.id }}</td >
+              <td>{{ user.firstname }}</td>
+              <td>{{ user.lastname }}</td>
+              <td>{{ user.email }}</td>
+              <td>
+               <button class="btn btn-danger" @click="deleteUser(user.id)">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table> 
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          firstname: 'John',
+          lastname: 'Doe',
+          email: 'john@gmail.com',
+        },
+      ],
+      newUser: {
+        id: null,
+        name: '',
+        email: ''
+      }
+    };
+  },
+  computed: {
+    totalUsers: function() {
+      return this.users.length;
+    }
+   
+  },
+  methods: {
+    addUser() {
+      this.users.push(this.newUser);
+      this.newUser = {
+        id: null,
+        name: '',
+        email: ''
+      };
+      this.newUser.id = this.users.length +1;
+    },
+     deleteUser(id) {
+    this.users = this.users.filter(user => user.id!== id);
+    }
+  }
+  
+  
+};
+</script>
